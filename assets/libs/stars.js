ddd7bdfca72c11bae31c397d984a0eab74104609
aifly <aifly_xc@163.com>
2017-08-01 22:39:57 +0800

    a

--- assets/libs/stars.js
+++ assets/libs/stars.js
@@ -1,18 +1,26 @@
 
-export default class Start {
-	constructor(props) {
-		
+export default class ZmitiStar {
+	constructor(options) {
+		this.x =  options.x;
+		this.y =  options.y;
+		this.speed =  options.speed;
+		this.img = options.img;
+		this.context = options.context;
+		this.width = options.context.canvas.width;
+		this.height = options.context.canvas.height;
 	}
 
 	init(){
-		
+		this.draw();
 	}
 
-	update(){
-	
+	update(i){
+		//this.context.clearRect(0,0,this.width,this.height);
+		this.context.drawImage(this.img,i*7,0,7,7,this.x,this.y,7,7);
 	}
-	draw(){
-		
+	draw(i){
+		var context = this.context;
+		context.drawImage(this.img,14,0,7,7,this.x,this.y,7,7);
 	}
 }
 
--- components/index/index.jsx
+++ components/index/index.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {PubCom} from '../public/pub.jsx';
 import './css/index.css';
-import $ from 'jquery';
+import  ZmitiStar from '../../assets/libs/stars';
 
 class ZmitiIndexApp extends Component {
 	constructor(props) {
@@ -22,8 +22,8 @@ class ZmitiIndexApp extends Component {
 				<header style={this.props.headerStyle}>
 					{this.props.title}
 					{this.props.date}
-					
 				</header>
+				<canvas width={this.viewW} height={this.viewH/3*2} ref='canvas'></canvas>
 			</div>
 		);
 	}
@@ -33,8 +33,54 @@ class ZmitiIndexApp extends Component {
 		setTimeout(()=>{
 			this.setState({
 				showimg:true
-			})
+			});
 		},2000);
+
+		var canvas = this.refs['canvas'];
+
+		var context = canvas.getContext('2d');
+		var img = new Image();
+		var width = canvas.width,
+			height = canvas.height;
+		var arr = [];
+		img.onload = function(){
+
+			for(var i=0;i<100;i++){
+				var star = new ZmitiStar({
+					context,
+					img,
+					x:Math.random()*this.viewW|0,
+					y:Math.random()*this.viewH|0
+				});
+				star.timer =0;
+				star.iNow = Math.random()*70+40;
+				star.speed = 0;
+				arr.push(star);
+
+				star.init();
+				
+			}
+		}.bind(this);
+
+		img.src= './assets/images/star.png'
+
+		var render = function(){
+
+			context.clearRect(0,0,width,height)
+			arr.map((star,i)=>{
+
+				star.timer += star.iNow;
+				if(star.timer>200){
+					star.timer=0;
+					star.speed+=1;
+					star.update(star.speed%7);
+				}
+			})
+			window.requestAnimationFrame(render);
+		}.bind(this);
+
+		render();
+		
 	}
 }
 export default PubCom(ZmitiIndexApp);
